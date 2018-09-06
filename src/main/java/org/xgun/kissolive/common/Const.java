2c898ceac940f4c77b8462fd8d30548db31a94b9
InnocentLEE <13822866200@163.com>
2018-09-06 11:37:17 +0800

    添加响应对象类(包括响应码枚举类)、常量类

---
+++ src/main/java/org/xgun/kissolive/common/Const.java
@@ -0,0 +1,10 @@
+package org.xgun.kissolive.common;
+
+/**
+ * Created by Lee on 2018/5/7.
+ */
+public class Const {
+    public static final String CURRENT_USER = "currentUser";
+    public static final String TELPHONE = "telphone";
+    public static final String USERNAME = "username";
+}
