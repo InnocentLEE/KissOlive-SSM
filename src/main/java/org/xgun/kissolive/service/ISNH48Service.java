010c199c0a511a3bd61e17f9c5c5d336201560b4
ZC <coderliuzc@gmail.com>
2018-10-16 17:25:56 +0800

    VIP信息列表和修改接口

--- src/main/java/org/xgun/kissolive/service/ISNH48Service.java
+++ src/main/java/org/xgun/kissolive/service/ISNH48Service.java
@@ -4,6 +4,7 @@ import org.xgun.kissolive.common.ServerResponse;
 import org.xgun.kissolive.pojo.Permit;
 import org.xgun.kissolive.pojo.Stock;
 import org.xgun.kissolive.pojo.Supplier;
+import org.xgun.kissolive.pojo.VipLevel;
 import org.xgun.kissolive.vo.ListOrder;
 import org.xgun.kissolive.vo.ListOrderItem;
 import org.xgun.kissolive.vo.ListStock;
@@ -36,4 +37,11 @@ public interface ISNH48Service {
 
     //判断用户是否拥有权限（0：homeManage；1：brandManage；2：goodsManage；3：stockManage；4：orderManage；5：activityManage）
     boolean hasPermit(Integer userID, Integer permitCode);
+
+    ServerResponse<List<VipLevel>> getVipLevel();
+
+    ServerResponse updateVipLevel(VipLevel vipLevel);
+
+    //获取用户的VIP等级信息
+    VipLevel getVIPLevel(Integer userID);
 }
