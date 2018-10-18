c09764753277ef2cc06efffc83b38cc4534a70b8
InnocentLEE <13822866200@163.com>
2018-10-18 15:29:02 +0800

    ....

--- src/main/java/org/xgun/kissolive/Zqu004Application.java
+++ src/main/java/org/xgun/kissolive/Zqu004Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.mybatis.spring.annotation.MapperScan;
 
@@ -62,4 +63,25 @@ public class Zqu004Application {
     String info(){
         return "page/user/userinfo.html";
     }
+
+	@RequestMapping("/production_detail")
+	String productionDetail(){
+		return "page/user/user_productiondetail.html";
+	}
+
+	@RequestMapping("/order_detail")
+	String orderDetail(){
+		return "page/user/user_detailorder.html";
+	}
+
+	@RequestMapping("/pay_success")
+	String paySuccess(){
+		return "page/user/user_successed.html";
+	}
+
+	@RequestMapping("/order_list")
+	String orderList(){
+		return "page/user/user_orderlist.html";
+	}
+
 }
