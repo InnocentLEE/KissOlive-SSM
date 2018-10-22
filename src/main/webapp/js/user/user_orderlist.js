bd275458a8d17d5a4bd619bca370272ed815b76c
Administrator <1029883270@qq.com>
2018-10-22 09:18:25 +0800

    订单详情

--- src/main/webapp/js/user/user_orderlist.js
+++ src/main/webapp/js/user/user_orderlist.js
@@ -8,6 +8,7 @@ var orderlist = new Vue({
         orderlist3:[],
         orderlist4:[],
         orderlist5:[],
+        orderlist0:[],
         page:1,
         size:100
     },
@@ -17,6 +18,7 @@ var orderlist = new Vue({
         this.getOrderlist(2);
         this.getOrderlist(3);
         this.getOrderlist(4);
+        this.getOrderlist(-1);
     },
     methods:{
         getOrderlist:function (status) {
@@ -26,16 +28,64 @@ var orderlist = new Vue({
             window.location.href="http://localhost:8080/order_detail?id="+id;
         },
         cancelOrder:function (id) {
-            alert("已取消");
+            var text="确定取消该订单吗?";
+            document.getElementById('show_msg').innerHTML=text;
+            $('#tip').addClass('bbox');
+            $('#tipbtn').one('click',function(){
+                doCancel(id);
+            })
         },
         payOrder:function (id) {
             window.location.href="http://localhost:8080/pay?id="+id;
         },
         receivedOrder:function (id) {
-            alert("已确认");
+            var text="确定收货吗?";
+            document.getElementById('show_msg').innerHTML=text;
+            $('#tip').addClass('bbox');
+            $('#tipbtn').one('click',function(){
+                doReceived(id);
+            })
         }
     }
 })
+//弹出框取消按钮事件
+$('.popup_de .btn_cancel').click(function(){
+    $('.popup_de').removeClass('bbox');
+});
+//弹出框关闭按钮事件
+$('.popup_de .popup_close').click(function(){
+    $('.popup_de').removeClass('bbox');
+});
+function doReceived(id) {
+    $.ajax({
+        type:"put",
+        url:"http://localhost:8080/order/receive/"+id,
+        cache: false,
+        dataType:'json',
+        success :function (data) {
+            if(data.status==0){
+                window.location.reload();
+            }else{
+                alert(data.msg);
+            }
+        }
+    })
+}
+function doCancel(id) {
+    $.ajax({
+        type:"put",
+        url:"http://localhost:8080/order/cancel/"+id,
+        cache: false,
+        dataType:'json',
+        success :function (data) {
+            if(data.status==0){
+               window.location.reload();
+            }else{
+                alert(data.msg);
+            }
+        }
+    })
+}
 function doGetOrderlist(status) {
     $.ajax({
         type:"get",
@@ -54,6 +104,8 @@ function doGetOrderlist(status) {
                     orderlist.orderlist4 = data.data;
                 else if(status==4)
                     orderlist.orderlist5 = data.data;
+                else if(status==-1)
+                    orderlist.orderlist0= data.data;
             }else{
                 alert(data.msg);
             }
